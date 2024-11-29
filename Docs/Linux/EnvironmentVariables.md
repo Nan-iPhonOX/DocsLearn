af87496bd7e945143724ef27ae69d81d266d2f73
CrazyPlant <380553426@qq.com>
2024-11-29 14:16:02 +0800

    learn 2025-1月

---
+++ Docs/Linux/EnvironmentVariables.md
@@ -0,0 +1,8 @@
+查看PATH环境变量的值
+
+```bash
+
+# 设置psql环境变量
+export LD_LIBRARY_PATH=/usr/local/pgsql17.2/lib
+export PATH=/usr/local/pgsql17.2/bin:$PATH
+```
--- sqlLink.sql
+++ sqlLink.sql
@@ -1,4 +1,42 @@
--- Active: 1723167065939@@127.0.0.1@1435@newdata
--- 创建新的数据库
-CREATE DATABASE Quesitions;
 
+-- 创建新的数据库
+CREATE DATABASE learn;
+
+CREATE DATABASE learn1 with OWNER = crazy ENCODING = 'utf-8';
+
+-- 修改数据库
+ALTER DATABASE learn RENAME TO learn2;
+-- 删除数据库
+DROP DATABASE learn;
+
+-- 创建数据表
+
+CREATE Table student(
+    id SERIAL,
+    name VARCHAR(20),
+    age INT
+)
+
+-- 添加约束
+alter table student alter name set not null;
+
+-- 修改数据类型
+ALTER TABLE student 
+    ALTER COLUMN id TYPE SERIAL;
+    
+--移除主键
+alter table student drop constraint id;
+
+-- 删除数据表
+DROP Table student;
+
+-- 插入数据
+INSERT INTO student(name,age) VALUES ('vgsj',18);
+
+-- 删除数据
+DELETE FROM student WHERE age = 18;
+
+-- 修改数据
+UPDATE student SET age = 18 WHERE age = 19;
+ 
+SELECT * FROM student;
